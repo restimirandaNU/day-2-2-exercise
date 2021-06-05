a23764adf5fa478f06f330b6bbe75da03bec8e07
resti973 <resti.mirandaNU@gmail.com>
2021-06-05 20:21:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in inches: ")
+weight = input("enter your weight in lbs: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+bmi = int(weight) / float(height) ** 2 * 703
+bmi_as_int = int(bmi)
+print(bmi_as_int)
+
+
+
+
+
+
+
+
